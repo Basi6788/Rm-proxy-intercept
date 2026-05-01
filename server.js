7d8678af2e1579a42c01d829b3fe2995fccba030
Basi6788 <chick786556@gmail.com>
2026-05-01 20:15:04 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-const TARGET_URL = 'https://version.astutech.online';
+const TARGET_URL = 'https://version.astutech.online/';
 
 // In-memory array logs store karne ke liye
 let requestLogs = []; 
